7c1cca544578f7c3cd0d954884d54f7d5c15ff47
Maximiliano Paolucci <maximiliano.paolucci@vodafone.com>
2020-06-25 08:30:18 +1200

    lesson 17

--- plinko/score.js
+++ plinko/score.js
@@ -1,8 +1,39 @@
+const outputs = [];
+const predictionPoint = 300;
+const k = 3;
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // Ran every time a balls drops into a bucket
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
+  
 }
 
 function runAnalysis() {
   // Write code here to analyze stuff
+  const bucket = _.chain(outputs)
+    .map(row => [distance(row[0]), row[3]])
+    .sortBy(row => row[0])
+    .slice(0, k)
+    .countBy(row => row[1])
+    .toPairs()
+    .sortBy(row => row[1])
+    .last()
+    .first()
+    .parseInt()
+    .value()
+
+  console.log('Your point will probably fall into', bucket);
 }
 
+function distance(point) {
+	return Math.abs(point - predictionPoint)
+}
+
+function splitDataset(data, testCount) {
+  const shuffled = _.shuffled(data);
+
+  const testSet = _.slice(shuffled, 0, testCount);
+  const trainingSet = _.slice(shuffled, testCount);
+
+  return [testSet, trainingSet];
+}
