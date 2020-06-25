f323a587ae104f45e8459c2b2764e5653b154162
Maxi Paolucci mac <maxipaolucci@gmail.com>
2020-06-25 13:53:34 +1200

    lesson 33

--- plinko/score.js
+++ plinko/score.js
@@ -1,6 +1,6 @@
 const outputs = [];
-const predictionPoint = 300;
-const k = 3;
+
+
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // Ran every time a balls drops into a bucket
@@ -9,9 +9,36 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 }
 
 function runAnalysis() {
-  // Write code here to analyze stuff
-  const bucket = _.chain(outputs)
-    .map(row => [distance(row[0]), row[3]])
+  const testSetSize = 100;
+  const k = 10;
+  
+
+  _.range(0,3).forEach(feature => {
+    const data = _.map(outputs, row => [row[feature], _.last(row)]);
+    const [testSet, trainingSet] = splitDataset(minMax(data, 1), testSetSize);
+    const accuracy = _.chain(testSet)
+    .filter(testPoint => knn(trainingSet, _.initial(testPoint), k) === _.last(testPoint))
+    .size() //amount of correct predictions
+    .divide(testSetSize)
+    .value();
+
+    console.log(`feature: ${feature} > Accuracy: ${accuracy}`);
+
+  });
+  
+
+  
+}
+
+function knn(data, point, k) {
+  return  _.chain(data)
+    .map(row => {
+      
+      return [
+        distance(_.initial(row), point), 
+        _.last(row)
+      ];
+    })
     .sortBy(row => row[0])
     .slice(0, k)
     .countBy(row => row[1])
@@ -20,20 +47,40 @@ function runAnalysis() {
     .last()
     .first()
     .parseInt()
-    .value()
-
-  console.log('Your point will probably fall into', bucket);
+    .value();
 }
 
-function distance(point) {
-	return Math.abs(point - predictionPoint)
+function distance(pointA, pointB) {
+  
+  return _.chain(pointA)
+    .zip(pointB)
+    .map(([a, b]) => (a - b) ** 2)
+    .sum()
+    .value() ** 0.5;
 }
 
 function splitDataset(data, testCount) {
-  const shuffled = _.shuffled(data);
+  const shuffled = _.shuffle(data);
 
   const testSet = _.slice(shuffled, 0, testCount);
   const trainingSet = _.slice(shuffled, testCount);
 
   return [testSet, trainingSet];
+}
+
+function  minMax(data, featurnCount) {
+  const clonedData = _.cloneDeep(data);
+
+  for (let i = 0; i < featurnCount; i++) {
+    const column = clonedData.map(row => row[i]);
+
+    const min = _.min(column);
+    const max = _.max(column);
+
+    for (let j = 0; j < clonedData.length; j++) {
+      clonedData[j][i] = (clonedData[j][i] - min) / (max - min);
+    }
+  }
+
+  return clonedData;
 }
